3b947066cf07216b3420162da3a41246ed5f46a4
manojkumartjpk <45778460+manojkumartjpk@users.noreply.github.com>
2024-01-17 19:39:23 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -2,19 +2,19 @@ import numpy as np
 
 INPUT_SCHEMA = {
     "prompt": {
-        'datatype': str,
+        'datatype': 'STRING',
         'required': True,
         'shape': [1],
         'example': ["There is a fine house in the forest"]
     },
     'negative_prompt': {
-        'datatype': str,
+        'datatype': 'STRING',
         'required': False,
         'example': ["test"],
         'shape': [1]
     },
     'width': {
-        'datatype': np.int8,
+        'datatype': 'INT8',
         'required': False,
         'example': [ 768 ,  512 ],
         'shape': [2]

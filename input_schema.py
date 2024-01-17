e09683e14cb44f2dd6e56a8becd60debed6d54fd
manojkumartjpk <45778460+manojkumartjpk@users.noreply.github.com>
2024-01-17 13:14:12 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -1,18 +1,18 @@
 INPUT_SCHEMA = {
     "prompt": {
-        'type': str,
+        'datatype': str,
         'required': True,
         'shape': [1],
         'example': ["There is a fine house in the forest"]
     },
     'negative_prompt': {
-        'type': str,
+        'datatype': str,
         'required': False,
         'example': None,
         'shape': [1]
     },
     'width': {
-        'type': int,
+        'datatype': int,
         'required': False,
         'example': [ 768 ,  512 ],
         'shape': [2]

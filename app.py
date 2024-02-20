159e54814aa7465774950b15128173b61fbd9168
manojkumartjpk <45778460+manojkumartjpk@users.noreply.github.com>
2024-02-20 17:52:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ class InferlessPythonModel:
         prompt = inputs["prompt"]
         pipeline_output = self.generator(prompt, do_sample=True, min_length=20)
         generated_txt = pipeline_output[0]["generated_text"]
-        return {"generated_text": generated_txt }
+        return {"generated_text": {"key1": "value1"} }
 
     # perform any cleanup activity here
     def finalize(self,args):

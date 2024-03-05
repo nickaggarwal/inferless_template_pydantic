d11c3a1727e0617500201dd91e3c166c54db06ee
manojkumartjpk <45778460+manojkumartjpk@users.noreply.github.com>
2024-03-05 08:08:36 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -1,20 +1,8 @@
 INPUT_SCHEMA = {
-    "prompt": {
+    "emails": {
         'datatype': 'STRING',
         'required': True,
-        'shape': [1],
-        'example': ["There is a fine house in the forest"]
-    },
-    'negative_prompt': {
-        'datatype': 'STRING',
-        'required': False,
-        'example': ["test"],
-        'shape': [1]
-    },
-    'width': {
-        'datatype': 'INT8',
-        'required': False,
-        'example': [ 768 ,  512 ],
-        'shape': [2]
-    },
+        'shape': [-1],
+        'example': ["test@inferless.com", "test1@inferless.com"]
+    }
 }

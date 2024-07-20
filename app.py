badab490e2646ded276c831c74d4f08205c4a99f
manojkumartjpk <45778460+manojkumartjpk@users.noreply.github.com>
2024-07-20 10:36:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ class InferlessPythonModel:
     # replace ##task_type## and ##huggingface_name## with appropriate values
     def initialize(self):
         self.generator = pipeline("text-generation", model="EleutherAI/gpt-neo-125M",device=0)
-        file_path = os.getenv("FILE_PATH")
+        file_path = os.getenv("FILE_PATH", "/var/test_apr_19.txt")
         if os.path.exists(file_path):
             print("File exists.", flush=True)
         else:
